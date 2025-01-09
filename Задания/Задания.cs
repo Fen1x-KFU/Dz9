e604818d66a30829572b8332829d1e90ace6deaa
Андрей Мингалёв <ammingalev@gmail.com>
2025-01-10 00:00:06 +0300

    ALL

--- Задания/Задания.cs
+++ Задания/Задания.cs
@@ -1,30 +1,4 @@
 ﻿using System;
 
-public class Singleton
-{
-    private Singleton() {}
-    public static Singleton source = null;
-    public static Singleton Source
-    {
-        get
-        {
-            if (source == null)
-            {
-                source = new Singleton();
-            }
-        return source;
-        }
-    }
-}
-
-public class Program
-{
-    static void Main()
-    {
-        Singleton bob = new Singleton();
-    }
-}
-
-
 
 
---
+++ Задания/Классы, интерфейсы и допфайлы/Классы.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania
+{
+    internal class Bich : Game
+    {
+        public void Play()
+        {
+            Игры game = (Игры)0;
+            Console.WriteLine($"Играть будем в {game}");
+        }
+    }
+
+    internal class Mousetrap : Game
+    {
+        public void Play()
+        {
+            Игры game = (Игры)1;
+            Console.WriteLine($"Играть будем в {game}");
+        }
+    }
+
+    internal class Sea : Game
+    {
+        public void Play()
+        {
+            Игры game = (Игры)2;
+            Console.WriteLine($"Играть будем в {game}");
+        }
+    }
+
+    internal class Fishing : Game
+    {
+        public void Play()
+        {
+            Игры game = (Игры)3;
+            Console.WriteLine($"Играть будем в {game}");
+        }
+    }
+
+    internal class Postmen : Game
+    {
+        public void Play()
+        {
+            Игры game = (Игры)4;
+            Console.WriteLine($"Играть будем в {game}");
+        }
+    }
+
+    internal class Slide : Game
+    {
+        public void Play()
+        {
+            Игры game = (Игры)5;
+            Console.WriteLine($"Играть будем в {game}");
+        }
+    }
+
+    internal class RobberCossacks : Game        
+    {
+        public void Play()
+        {
+            Игры game = (Игры)6;
+            Console.WriteLine($"Играть будем в {game}");
+        }
+    }
+
+    internal class Team
+    {
+        protected int players; 
+        protected string countries;
+
+        public Team(int players, string countries, Game game)
+        {
+            this.players = players;
+            this.countries = countries;
+            Games = game;
+        }
+        public Game Games { private get; set; }
+        public void Games()
+        {
+            Games.Play();
+        }
+    }
+}
